1442df0b766bb782af8051a1dbe8562a9e7af81f
Austin Carr-Jones <austincarr.jones@gmail.com>
2024-04-12 08:24:19 -0600

    stuck here approx 2hrs 50 mins total work time

--- lib/museum.rb
+++ lib/museum.rb
@@ -30,4 +30,16 @@ class Museum
     @patrons << patron
   end
 
+  def patrons_by_exhibit_interest
+    #return hash
+    #each key is an exhibit
+    #value is an array of patrons with interest in it
+    #look at each exhibit, and match with patrons who have interest in it
+    exhibit_interest = {}
+    @exhibits.each do |exhibit|
+      exhibit_interest[:exhibit] = []
+      binding.pry
+    end
+  end
+
 end
